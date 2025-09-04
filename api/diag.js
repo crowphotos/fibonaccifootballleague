494e27bb8ffc8c7dea8c5b56be6e39debf7d1ec3
crowphotos <crowphotographs@gmail.com>
2025-09-04 13:25:14 -0700

    diags

--- api/diag.js
+++ api/diag.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
-  const hasUrl = !!process.env.POSTGRES_URL;
-  res.status(200).json({
-    ok: true,
-    node: process.version,
-    hasPostgresUrl: hasUrl
-  });
+  const env = {
+    has_POSTGRES_URL: Boolean(process.env.POSTGRES_URL),
+    has_POSTGRES_URL_NON_POOLING: Boolean(process.env.POSTGRES_URL_NON_POOLING),
+    node: process.version
+  };
+  res.status(200).json({ ok: true, env });
 }
 
