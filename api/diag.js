23374eaf386bed8e9c6bf31245b8bf130921417e
crowphotos <crowphotographs@gmail.com>
2025-09-06 11:32:06 -0700

    Adding a cors wrapper.

--- api/diag.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const env = {
-    has_POSTGRES_URL: Boolean(process.env.POSTGRES_URL),
-    has_POSTGRES_URL_NON_POOLING: Boolean(process.env.POSTGRES_URL_NON_POOLING),
-    node: process.version
-  };
-  res.status(200).json({ ok: true, env });
-}
-
